9a68b3f22f2a5e579200718595848476384be5ea
Clark <daprogrammer1@gmail.com>
2022-05-29 01:24:49 +0800

    updates

--- src/utils/isHighScore.ts
+++ src/utils/isHighScore.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config'
-import mongoose from 'mongoose'
 import { User } from './Schema'
+import { connectDB } from './connectDB' 
 
 // mongoose implementation
 const fetchInfo = async (userLoggedIn: string) => {
+  connectDB()
   let info = await User.findOne({ name: userLoggedIn })
   return info
 }
@@ -20,36 +21,6 @@ const isHighScore = async (user_score: number, userLoggedIn: string) => {
     } 
   } 
 }
-
-/*const fetchInfo = async (userLoggedIn: string) => {
-  let info = await prisma.user.findFirst({
-    where: {
-      name: userLoggedIn
-    }
-  })
-  return info
-}
-
-const isHighScore = async (user_score: number, userLoggedIn: string) => {
-  const info: User | null = await fetchInfo(userLoggedIn)
-  if (info) {
-    if (user_score > info.highscore) {
-      console.log(`New high score: ${user_score}`)
-      await prisma.user.update({
-        where: {
-          name: info.name
-        },
-        data: {
-          highscore: user_score
-        }
-      })
-    } 
-  } 
-}*/
-
-
-
-
 
 // const main = (s: string) => {
 //   isHighScore(2,s)
